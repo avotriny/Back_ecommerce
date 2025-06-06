8f75fb8409455775e3b9bd0ea201f12f79432b45
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-06 09:02:06 +0200

    Modele commande

---
+++ app/Models/Commande.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commande extends Model
+{
+    use HasFactory;
+    protected $table='commandes';
+    protected $fillable = [
+        'prod_id',
+        'prix_total',
+        'quantite',
+        'nom',
+        'email',
+        'phone',
+        'adresse',
+        'status',
+        'latitude',
+        'longitude',
+    ];
+    public function produit(){
+        return $this->belongsTo(Produit::class, 'prod_id', 'id');
+    }
+}
