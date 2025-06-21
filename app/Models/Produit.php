f8d3ca0a404b54ae9e31932c78c273eecf5072fa
Avotriniaina <avotriniainajonatan@gmail.com>
2025-06-21 08:12:36 +0200

    Produit Liké

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -8,22 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Produit extends Model
 {
     use HasFactory;
-     protected $fillable = [
+
+    protected $fillable = [
         'subcat_id', 'nom_prod', 'desc_prod', 'prix_prod', 'images',
-        'poids_prod', 'origin_prod', 'stock_prod', "promotion","prix_promo","couleur","taille", "pointure","status"
+        'poids_prod', 'origin_prod', 'stock_prod', 'promotion', 'prix_promo', 'couleur', 'taille', 'pointure', 'status'
     ];
 
     protected $casts = [
-        'images' => 'array', // Cast images as an array
+        'images' => 'array',      // Cast images as array
+        'status' => 'boolean',    // Cast status as boolean
     ];
 
-
     public function subcategorie()
-{
-    return $this->belongsTo(Subcategorie::class, 'subcat_id', 'id');
-}
-public function commande()
+    {
+        return $this->belongsTo(Subcategorie::class, 'subcat_id');
+    }
+
+    public function commande()
     {
         return $this->belongsToMany(Commande::class);
+    }
+
+        public function likes()
+    {
+        return $this->hasMany(Like::class, 'prod_id');
     }
 }
